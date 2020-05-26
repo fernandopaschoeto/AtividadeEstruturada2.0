fc403c622c0b4a6208b839d6d80826d3ef50160c
LUIZ FERNANDO ARAUJO PASCHOETO <fernando.paschoeto@outlook.com>
2020-05-25 22:53:01 -0300

    Adicionado contagem de escolhas.

--- AE_2.0.cpp
+++ AE_2.0.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,10 +11,14 @@ int main ()
     float total, fabrica, trioEletrico, dirHidraulica, arCond, ipi, completo, desconto;
     char resposta;
     int escolha;
+    int opcao1 = 0;
+    int opcao2 = 0;
+    int opcao3 = 0;
     total = 0;
     completo = 0;
     resposta = 's';
     
+    cout << fixed << setprecision(2) << endl;
     cout << "Informe o valor de fábrica do veículo: ";
     cin >> fabrica;
     ipi = ((8*fabrica)/100);
@@ -38,16 +43,19 @@ int main ()
         {
             trioEletrico = ((2*(fabrica+ipi))/100);
             total = total + trioEletrico;
+            opcao1++;
         }
         if (escolha == 2)
         {
             arCond = ((2*fabrica)/100);
             total = total + arCond;
+            opcao2++;
         }
         if (escolha == 3)
         {
             dirHidraulica = ((2*fabrica)/100);
             total = total + dirHidraulica;
+            opcao3++;
         }
         if (escolha == 4)
         {
@@ -55,14 +63,21 @@ int main ()
             //total = ipi - completo;
             break;
         }
+       /* else
+        {
+            cout << "Operação Inválida!!" << endl;
+        }
+        */
         cout << "Deseja outro adicional? (s/n)" << endl;
         cin >> resposta;
     }
     desconto = ((3.5*completo)/100);
-    cout << fixed << setprecision(2);
     cout << "Veículo Básico: R$" << fabrica+ipi << " reais" << endl;
     cout << "Valor do IPI: R$" << ipi << " reais" <<endl;
     cout << "Veículo com adicionais: R$" << (total+fabrica+ipi) << " reais" << endl;
+    cout << "Você optou " << opcao1 << " vezes o Trio Elétrico." << endl;
+    cout << "Você optou " << opcao2 << " Vezes o Ar condicionado." << endl;
+    cout << "Você optou " << opcao3 << " Vezes a Direção hidraulica." << endl;
     cout << "Desconto para veículo completo: R$" << desconto << endl;
     cout << "Completo: R$" << (completo - desconto) << " reais" << endl;
 
